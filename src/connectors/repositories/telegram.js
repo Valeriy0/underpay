b543f235e21854f12c50885d98ad8b3cee6cf27f
Valeriy0 <v.kistrinov@yandex.ru>
2025-03-10 19:39:58 +0300

    update telegram fetch

--- src/connectors/repositories/telegram.js
+++ src/connectors/repositories/telegram.js
@@ -2,7 +2,7 @@ import { requestApi } from '../api';
 import { BaseRepository } from './base';
 
 class Repository extends BaseRepository {
-  getTelegramCode = (params) => requestApi('get', `${this.path}/request`, params);
+  getTelegramCode = (params) => requestApi('get', `${this.path}/auth`, params);
 }
 
 export const TelegramRepository = new Repository('/telegram');

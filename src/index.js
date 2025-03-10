4bcb506db17a16a41be70df9cbca6059c2699c38
Valeriy0 <v.kistrinov@yandex.ru>
2025-03-10 17:20:56 +0300

    add initial tg

--- src/index.js
+++ src/index.js
@@ -2,6 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
+import { init, miniApp } from '@telegram-apps/sdk';
+
+const initializeTelegramSDK = async () => {
+  try {
+    await init();
+
+
+    if (miniApp.ready.isAvailable()) {
+      await miniApp.ready();
+    }
+
+
+  } catch (error) {
+    console.error('Ошибка инициализации:', error);
+  }
+};
+
+initializeTelegramSDK();
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>

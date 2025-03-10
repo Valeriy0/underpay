815b9a018c36d95e404862bd2d19bdddb351dd99
Valeriy0 <v.kistrinov@yandex.ru>
2025-03-10 20:19:05 +0300

    update sizes

--- src/layouts/BaseLayout/index.jsx
+++ src/layouts/BaseLayout/index.jsx
@@ -3,8 +3,8 @@ import { BottomMenu } from '../../components/BottomMenu';
 
 export const BaseLayout = ({ children, className, withMenu = false }) => {
   return (
-    <div className="flex items-center justify-center bg-mainBg w-full h-full">
-      <div className={`relative flex flex-col items-center justify-start w-screen h-full max-w-[39rem] `}>
+    <div className="flex items-center justify-center bg-mainBg w-screen h-screen">
+      <div className={`relative flex flex-col items-center justify-start w-full h-full max-w-[39rem] `}>
         <div className={` flex flex-col items-center justify-start h-full w-full overflow-hidden overflow-y-auto invisible-scrollbar ${className}`}>
           {children}
         </div>

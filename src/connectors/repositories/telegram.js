1c9f1b5d380a0b2d96221a206586cfe1b7367350
Valeriy0 <v.kistrinov@yandex.ru>
2025-03-10 18:13:16 +0300

    add telegram auth

--- src/connectors/repositories/telegram.js
+++ src/connectors/repositories/telegram.js
@@ -2,7 +2,7 @@ import { requestApi } from '../api';
 import { BaseRepository } from './base';
 
 class Repository extends BaseRepository {
-  getTelegramSecretCode = (params) => requestApi('get', `${this.path}/request`, params);
+  getTelegramCode = (params) => requestApi('get', `${this.path}/request`, params);
 }
 
 export const TelegramRepository = new Repository('/telegram');
--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -1,8 +1,21 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { BaseLayout } from "../../layouts/BaseLayout";
 import { Link } from "react-router-dom";
+import { useRequest } from "../../helpers/hooks/useRequest";
+import { TelegramRepository } from "../../connectors/repositories/telegram";
+import { initData } from '@telegram-apps/sdk';
 
 export const Login = () => {
+    const { data, call, isLoading } = useRequest(TelegramRepository.getTelegramCode);
+
+    console.log(JSON.stringify(initData), 'init data');
+
+    useEffect(() => {
+        if (initData) {
+          call([JSON.stringify(initData)]);
+        }
+      }, [initData]);
+
     return (
         <BaseLayout className='px-4 py-9'>
             <img className="max-w-[30.2rem] w-full mb-9" src="/images/login/preview.webp" alt="" />

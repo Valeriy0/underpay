2867eb52116b10353942e12671a9e475f9cdb254
Valeriy0 <v.kistrinov@yandex.ru>
2025-03-10 20:09:45 +0300

    update

--- src/connectors/repositories/telegram.js
+++ src/connectors/repositories/telegram.js
@@ -2,7 +2,7 @@ import { requestApi } from '../api';
 import { BaseRepository } from './base';
 
 class Repository extends BaseRepository {
-  getTelegramCode = (params) => requestApi('get', `${this.path}/auth`, params);
+  getTelegramCode = (params) => requestApi('post', `${this.path}/auth`, params);
 }
 
 export const TelegramRepository = new Repository('/telegram');
--- src/layouts/BaseLayout/index.jsx
+++ src/layouts/BaseLayout/index.jsx
@@ -3,8 +3,8 @@ import { BottomMenu } from '../../components/BottomMenu';
 
 export const BaseLayout = ({ children, className, withMenu = false }) => {
   return (
-    <div className="flex items-center justify-center">
-      <div className={`relative bg-mainBg flex flex-col items-center justify-start w-screen h-screen max-w-[39rem] `}>
+    <div className="flex items-center justify-center bg-mainBg w-full h-full">
+      <div className={`relative flex flex-col items-center justify-start w-screen h-screen max-w-[39rem] `}>
         <div className={` flex flex-col items-center justify-start h-full w-full overflow-hidden overflow-y-auto invisible-scrollbar ${className}`}>
           {children}
         </div>

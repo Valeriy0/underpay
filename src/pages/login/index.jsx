26bb181844623a3a56dc9b01ea8b5c5ecebe0be7
«Valer000n» <valeriy0@csfail.com>
2025-03-11 14:02:04 +0300

    test console

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -19,17 +19,17 @@ export const Login = () => {
           const userData = {
             id: user.id.toString(),
             first_name: user.first_name,
-            username: user.username || '', // username может отсутствовать
-            photo_url: user.photo_url || '', // photo_url может отсутствовать
-            auth_date: launchParams.auth_date.toString(), // Дата аутентификации
-            hash: launchParams.hash, // Хеш для проверки данных
+            username: user.username || '',
+            photo_url: user.photo_url || '',
+            auth_date: launchParams.auth_date.toString(), 
+            hash: launchParams.hash, 
           };
     
           setUserData(userData);
         }
       }, []);
 
-      console.log(userData);
+      console.log(userData, JSON.stringify(userData));
 
       useEffect(() => {
         const sendData = async () => {

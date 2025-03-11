877c17f4e751872647b660a060e7b3e7f910da80
«Valer000n» <valeriy0@csfail.com>
2025-03-11 13:16:22 +0300

    update

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { RoutesFind } from "./RoutesFind";
 import './style.scss';
-import { useEffect } from 'react';
+import { useState,  useEffect } from 'react';
 import axios from 'axios';
 import { initData } from '@telegram-apps/sdk';
 
@@ -14,12 +14,12 @@ function App() {
       const user = initData?.user();
       console.log('user enable')
       const userData = {
-        id: user.id.toString(),
-        first_name: user.first_name,
-        username: user.username || '',
-        photo_url: user.photo_url || '',
-        auth_date: initData.auth_date().toString(),
-        hash: initData.hash(),
+        id: user?.id?.toString(),
+        first_name: user?.first_name,
+        username: user?.username || '',
+        photo_url: user?.photo_url || '',
+        auth_date: initData?.auth_date()?.toString(),
+        hash: initData?.hash(),
       };
 
       setUserData(userData);
@@ -27,23 +27,23 @@ function App() {
 
   }, [initData]);
 
-  console.log(initData, userData);
+  console.log(userData);
 
-  useEffect(() => {
-    const sendData = async () => {
-      try {
-        const response = await axios.post('https://0b5e-171-97-245-69.ngrok-free.app/telegram/auth', JSON.stringify(initData));
+  // useEffect(() => {
+  //   const sendData = async () => {
+  //     try {
+  //       const response = await axios.post('https://0b5e-171-97-245-69.ngrok-free.app/telegram/auth', JSON.stringify(initData));
 
-        console.log('Response:', response.data);
-      } catch (error) {
-        console.error('Error:', error);
-      }
-    };
+  //       console.log('Response:', response.data);
+  //     } catch (error) {
+  //       console.error('Error:', error);
+  //     }
+  //   };
 
-    if (initData) {
-      sendData();
-    }
-  }, [initData]);
+  //   if (initData) {
+  //     sendData();
+  //   }
+  // }, [initData]);
 
   return (
     <RoutesFind />

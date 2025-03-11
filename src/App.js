523e9c672b9e9856d97c47650232c5b61d6befc7
«Valer000n» <valeriy0@csfail.com>
2025-03-11 11:24:08 +0300

    test

--- src/App.js
+++ src/App.js
@@ -1,8 +1,27 @@
 import React from "react";
 import { RoutesFind } from "./RoutesFind";
 import './style.scss';
+import { retrieveLaunchParams } from '@tma.js/sdk';
 
 function App() {
+
+  const initData = retrieveLaunchParams();
+
+  useEffect(() => {
+    const sendData = async () => {
+      try {
+        const response = await axios.post('https://gogt1tcrfq.loclx.io/api/telegram/auth', JSON.stringify(initData));
+
+        console.log('Response:', response.data);
+      } catch (error) {
+        console.error('Error:', error);
+      }
+    };
+
+    if (initData) {
+      sendData();
+    }
+  }, [initData]);
 
   return (
     <RoutesFind />

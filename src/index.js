0ec430dee641aefdcd4d301aaf6fe10cfef3e2c1
«Valer000n» <valeriy0@csfail.com>
2025-03-11 12:49:16 +0300

    revert

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { init, miniApp, initData, initDataUser } from '@telegram-apps/sdk';
 import { isTMA } from '@telegram-apps/bridge';
-import axios from 'axios';
 
 const initializeTelegramSDK = async () => {
   try {
@@ -12,7 +11,8 @@ const initializeTelegramSDK = async () => {
 
     if (miniApp.ready.isAvailable()) {
       await miniApp.ready();
-      sendData(initDataUser())
+
+      console.log(1234, initDataUser(), initData);
     }
 
 
@@ -21,16 +21,9 @@ const initializeTelegramSDK = async () => {
   }
 };
 
-const sendData = async (initData) => {
-  console.log('sended', JSON.stringify(initData), initData)
-  try {
-    const response = await axios.post('https://gogt1tcrfq.loclx.io/telegram/auth', JSON.stringify(initData));
-
-    console.log('Response:', response.data);
-  } catch (error) {
-    console.error('Error:', error);
-  }
-};
+if (await isTMA()) {
+  console.log(1234, initDataUser(), initData);
+}
 
 initializeTelegramSDK();
 

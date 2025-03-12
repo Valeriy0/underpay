7a7181146e7719b06d4b75990acb9563f8b0522f
Valeriy0 <v.kistrinov@yandex.ru>
2025-03-13 00:19:32 +0300

    test131442134

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { init, miniApp, initData } from '@telegram-apps/sdk';
+import { init, miniApp, initData, initDataRaw } from '@telegram-apps/sdk';
 
 
 const initializeTelegramSDK = async () => {
@@ -11,7 +11,7 @@ const initializeTelegramSDK = async () => {
 
     if (miniApp.ready.isAvailable()) {
       await miniApp.ready();
-      console.log(initData, initData.raw());
+      console.log(initData, initDataRaw());
     }
 
 

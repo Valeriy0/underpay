2def6c6d51f5642abae4ffab4d103aa4b8b72cca
Valeriy0 <v.kistrinov@yandex.ru>
2025-03-17 01:01:16 +0300

    test

--- src/pages/profile/index.jsx
+++ src/pages/profile/index.jsx
@@ -17,7 +17,6 @@ export const Profile = () => {
 
     return (
         <BaseLayout withMenu className="px-[1.6rem]">
-            {data?.data?.user?.balanceUsd}
             <Info />
             <Balance balance={data?.data?.user?.balanceUsd || 0} />
             <Menu />

242580411206ee6fc0e9198df074841f4a26f94b
Valeriy0 <v.kistrinov@yandex.ru>
2025-03-17 00:52:59 +0300

    fix

--- src/pages/profile/index.jsx
+++ src/pages/profile/index.jsx
@@ -18,7 +18,7 @@ export const Profile = () => {
     return (
         <BaseLayout withMenu className="px-[1.6rem]">
             <Info />
-            <Balance balance={data?.data?.balanceUsd} />
+            <Balance balance={data?.data?.user?.balanceUsd} />
             <Menu />
         </BaseLayout>
     )

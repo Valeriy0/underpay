dff68fec5e5c3d0f393f8036d36066d6b08695f5
«Valer000n» <valeriy0@csfail.com>
2025-03-21 01:46:18 +0300

    TEST BACKBUTOTN

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { RoutesFind } from "./RoutesFind";
 import './style.scss';
+import { useInitData } from "@tma.js/sdk-react";
 
 
 function App() {
+  const initData = useInitData();
+
+  useEffect(() => {
+    if (initData && initData.init) {
+      initData.init();
+    }
+  }, [initData]);
+
   return (
     <RoutesFind />
   );

53f53ae2d8026c99c036fa71ed163bdce7daa22c
«Valer000n» <valeriy0@csfail.com>
2025-03-23 17:04:29 +0300

    add

--- src/helpers/config.js
+++ src/helpers/config.js
@@ -1,5 +1,6 @@
 // eslint-disable-next-line import/no-anonymous-default-export
 export default {
   apiUrl: process.env.REACT_APP_API_URL,
+  backendUrl: process.env.REACT_APP_BACKEND_URL,
   stand: process.env.REACT_APP_STAND,
 };

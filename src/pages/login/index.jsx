ab9d32b8910bd921b5ad3e6bfc400546172388f8
«Valer000n» <valeriy0@csfail.com>
2025-03-23 17:31:50 +0300

    tst

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -27,7 +27,7 @@ export const Login = () => {
         const initData = retrieveLaunchParams();  
         try {
           setIsLoading(true);
-          const response = await axios.post(config.apiUrl + 'telegram/auth', JSON.stringify(initData));
+          const response = await axios.post('https://wi0bbzhfzm.eu.loclx.io/api/telegram/auth', JSON.stringify(initData));
 
           if (response.data) {
             setCookie(null, 'apiToken', response.data.data.jwtToken, {

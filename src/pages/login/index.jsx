4e21077350360cf86db733d4f2beeaee14f361ed
Valeriy0 <v.kistrinov@yandex.ru>
2025-03-29 14:28:11 +0300

    обновить URL-адреса бэкенда и API в файлах конфигурации окружения и страницы входа

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -27,7 +27,7 @@ export const Login = () => {
         const initData = retrieveLaunchParams();  
         try {
           setIsLoading(true);
-          const response = await axios.post('https://5519-77-221-153-239.ngrok-free.app/api/telegram/auth', JSON.stringify(initData));
+          const response = await axios.post('https://4d87-77-221-153-239.ngrok-free.app/api/telegram/auth', JSON.stringify(initData));
 
           if (response.data) {
             setCookie(null, 'apiToken', response.data.data.jwtToken, {
